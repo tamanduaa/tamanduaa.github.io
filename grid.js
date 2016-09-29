083f5caf3fac5b4a123a0ddb139c8b86cd5142e9
lperry <laurellperry@gmail.com>
2016-09-28 23:01:26 -0700

    masonry

--- grid.js
+++ grid.js
@@ -1,7 +1,7 @@
+// external js: masonry.pkgd.js
+
 $('.grid').masonry({
-  // set itemSelector so .grid-sizer is not used in layout
   itemSelector: '.grid-item',
-  // use element for option
   columnWidth: '.grid-sizer',
   percentPosition: true
-})
+});
