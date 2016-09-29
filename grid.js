dc046b71f022c6bc23a310c476817e76e414be4d
lperry <laurellperry@gmail.com>
2016-09-28 22:56:09 -0700

    masonry

--- grid.js
+++ grid.js
@@ -1,5 +1,7 @@
 $('.grid').masonry({
-  // options
+  // set itemSelector so .grid-sizer is not used in layout
   itemSelector: '.grid-item',
-  columnWidth: 200
-});
+  // use element for option
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+})
